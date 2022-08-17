198b60cd45bd62b12307f92d7cb37016215031fc
Wissam Antoun <wissam.antoun@gmail.com>
2022-08-17 16:47:45 +0200

    fixes tab bugs, refactoring

--- stt_web_app.py
+++ stt_web_app.py
@@ -1,152 +1,135 @@
-from ast import Delete
-from distutils.command.upload import upload
-from turtle import onclick
 import streamlit as st
 import speech_recognition as sr
 from pydub import AudioSegment
-import winsound
+import asyncio
+from copy import deepcopy
+from pathlib import Path
+
 # import asyncio
-from functions import send_email
-
-# Initializing page elements
-
-title = st.container()
-input = st.container()
-language = st.container()
-mode = st.container()
-tab1, tab2 = st.tabs(['Live', 'Recorded'])
-button_container = st.container()
-button_columns = st.columns([1,1])
-output = st.container()
-output_options = st.columns([1,1,1])
-txt=''
+from send_email import send_email
 
 # Control variables
-PH = 'Speak or upload a file to display the corresponding transcription'
+PH = "Speak or upload a file to display the corresponding transcription"
 # Session state
-if 'text' not in st.session_state:
-    st.session_state['text'] = ''
-    st.session_state['run'] = False
+if "text" not in st.session_state:
+    st.session_state["text"] = ""
+    st.session_state["run"] = False
 
-if 'lang' not in st.session_state:
-    st.session_state['lang'] = 'ar-LB'
-
-# Defining necessary functions
+if "lang" not in st.session_state:
+    st.session_state["lang"] = "ar-LB"
 
 
-# def start_listening():
-#     st.session_state['run'] = True
-#     st.session_state['text'] = 'Listening...'
 def extract_text_fromfile():
-    if uploaded_file is None:
-        st.warning('Please upload a file first')
+    if st.session_state["uploaded_file"] is None:
+        st.warning("Please upload a file first")
     else:
-        if uploaded_file.name[len(uploaded_file.name) -3:] == 'mp3':
-            sound = AudioSegment.from_mp3(uploaded_file)
-            uploaded_file = sound.export('test.wav',format="wav")
-            # with open('myfile.wav', mode="bw") as f:
-            #     f.write(uploaded_file.getbuffer)
+        uploaded_file = st.session_state["uploaded_file"]
 
-        # if uploaded_file.name[len(uploaded_file.name) -3:]=='mp4':
-        #     #src
-        src=sr.AudioFile(uploaded_file)
-        r=sr.Recognizer()
+        if Path(uploaded_file.name).suffix != ".wav":
+            sound = AudioSegment.from_file(uploaded_file)
+            uploaded_file = sound.export(format="wav")
+
+        src = sr.AudioFile(uploaded_file)
+        r = sr.Recognizer()
         with src as source:
-            audio=r.record(source)
-        val=r.recognize_google(audio)
-        st.session_state['text'] = val
-        
+            audio = r.record(source)
+        val = r.recognize_google(audio, language=st.session_state["lang"])
+        st.session_state["text"] = val
+
 
 def playfile():
     if uploaded_file is None:
-        st.warning('Please upload a file first') 
+        st.warning("Please upload a file first")
     else:
-        if uploaded_file.name[len(uploaded_file.name) -3:]=='mp4':
+        if uploaded_file.name[len(uploaded_file.name) - 3 :] == "mp4":
             playvideo(uploaded_file)
         else:
-            playaudio(uploaded_file)
+            playaudio(deepcopy(uploaded_file))
+
 
 def playaudio(audio):
-        audio_bytes = audio.read()
-        with mode:
-            st.audio(audio_bytes, format='audio/ogg')
+    audio_bytes = audio.read()
+    st.audio(audio_bytes, format="audio/ogg")
+
 
 def playvideo(video):
-        video_bytes = video.read()
-        with mode:
-            st.video(video_bytes)
+    video_bytes = video.read()
+    st.video(video_bytes)
+
+
+def start_listening():
+    st.session_state["run"] = True
+    st.session_state["text"] = ""
 
 
 def stop_listening():
-    st.session_state['run'] = False
+    st.session_state["run"] = False
+
 
 def clear_text():
-    st.session_state['text'] = uploaded_file
-    winsound.PlaySound(uploaded_file, winsound. SND_FILENAME)
+    st.session_state["text"] = ""
 
-def recognize():
-    st.session_state['run'] = True
-    st.session_state['text'] = 'Listening...'
-    while st.session_state['run']:
+
+async def live_recognize():
+    with sr.Microphone() as source:
         rec = sr.Recognizer()
-        # Function to convert speech to text
-        with sr.Microphone() as source:
-            rec.adjust_for_ambient_noise(source, duration=0.5)
-            audio = rec.listen(source)
-            lang = st.session_state['lang']
+        rec.adjust_for_ambient_noise(source, duration=0.5)
+        while st.session_state["run"]:
+            st.markdown("Listening...")
+            audio = rec.listen(source, phrase_time_limit=10)
             try:
-                txt = rec.recognize_google(audio, language=lang)
-                st.session_state['text'] = txt
+                # TODO try await
+                result = rec.recognize_google(audio, language=st.session_state["lang"])
+                st.session_state["text"] += result + "\n"
+
             except sr.UnknownValueError:
-                txt = "Google Speech Recognition could not understand audio"
-                st.session_state['text'] = txt
+                st.markdown("Could not understand audio")
             except sr.RequestError as e:
-                txt = "Could not request results from Google Speech Recognition service; {0}".format(e)
-                st.session_state['text'] = txt
+                st.markdown(
+                    "Could not request results from speech recognition service; {0}".format(
+                        e
+                    )
+                )
+
 
 # Constructing page
-st.title('Speech Recognizer')
-with input:
-    with language:
-        LANG = st.selectbox('Please Select a Language', options=['ar-LB', 'en-US'])
-        st.session_state['lang'] = LANG
-    with mode:
-        with tab1:
-            with button_columns[0]:
-                if tab1:
-                    st.button('Start', on_click=recognize)
-            with button_columns[1]:
-                st.button('Stop', on_click=stop_listening)
-        with tab2:
-            uploaded_file = st.file_uploader("Choose a file",type=["wav","mp3","mp4","ogg"])
-            x = st.columns([1,1])
-            with x[0]:
-                if tab2:
-                    st.button('Play Uploaded File',on_click=playfile)
-            with x[1]:
-                st.button('Extract Text',on_click= extract_text_fromfile)
-with output:
-    st.text_area('Transcription', st.session_state['text'], placeholder=PH)
+st.title("Speech Recognizer")
+LANG = st.selectbox("Please Select a Language", options=["ar-LB", "en-US"])
+st.session_state["lang"] = LANG if LANG else "ar-LB"
 
-    with output_options[0]:
-        st.download_button(
-            label='Download text',
-            data=st.session_state['text'],
-            file_name='transcribed_text.txt')
-    with output_options[1]:
-        email = st.button('Send Email')
-    with output_options[2]:
-        st.button('Clear text', on_click=clear_text)
+tab1, tab2 = st.tabs(["Live", "Recorded"])
+with tab1:
+    if not st.session_state["run"]:
+        st.button("Start", on_click=start_listening)
+    else:
+        st.button("Stop", on_click=stop_listening)
+
+with tab2:
+    uploaded_file = st.file_uploader("Choose a file", type=["wav", "mp3", "mp4", "ogg"])
+    st.session_state["uploaded_file"] = uploaded_file
+    button_columns_tab2 = st.columns([1, 1])
+    if button_columns_tab2[0].button("Play Uploaded File"):
+        playfile()
+    button_columns_tab2[1].button("Extract Text", on_click=extract_text_fromfile)
 
 
+st.text_area("Transcription", st.session_state["text"], placeholder=PH)
 
-# asyncio.run(recognize())
+output_options = st.columns([1, 1, 1])
+output_options[0].download_button(
+    label="Download text",
+    data=st.session_state["text"],
+    file_name="transcribed_text.txt",
+)
+output_options[1].button("Send Email")
+output_options[2].button("Clear text", on_click=clear_text)
 
-# A session is started whenever an instance of Streamlit app is
-# opened in a browser. Each session exists as long as the tab is
-# opened. Opening 2 tabs will create two independent sessions.
-# A session is what happens in the browser and the state is how
-# we store the current values of widgets and parameters to use
-# later.
 
-
+if not st.session_state["run"] and st.session_state["text"]:
+    if st.session_state["lang"] == "en-US":
+        st.session_state["text"] = (
+            st.session_state["text"][0].upper() + st.session_state["text"][1:-1] + "."
+        )
+    else:
+        st.session_state["text"] = st.session_state["text"] + "."
+asyncio.run(live_recognize())
